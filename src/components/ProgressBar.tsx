2773e5d3f4ccdeaccda830661ebab345e3845f39
Max Forbang <max@Maxs-MacBook-Pro-2.local>
2023-07-06 23:07:19 -0400

    Remove dark progress bar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -3,7 +3,7 @@ import { classNames } from "~/utils/shared/functions";
 export function ProgressBar({ percentage }: { percentage: number }) {
   return (
     <div className="group relative">
-      <div className="h-2 w-full rounded-full bg-gray-200 transition-all duration-300 dark:bg-gray-700">
+      <div className="h-2 w-full rounded-full bg-gray-200 transition-all duration-300">
         <span
           className={classNames(
             "absolute bottom-5 right-0 z-10 scale-0 rounded p-2 text-lg font-bold tracking-wide text-white duration-150 group-hover:scale-100 sm:right-16",
